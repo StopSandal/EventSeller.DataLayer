8a8fabc264aa9cfd429552c1c97d7d3461db2183
Кирилл Солдатенко <soldatenko.kirillas@gmail.com>
2024-06-04 19:28:00 +0300

    add JWT token

---
+++ EntitiesViewModel/LoginUserVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventSeller.DataLayer.EntitiesViewModel
+{
+    public class LoginUserVM
+    {
+        [Required]
+        [DisplayName("User name")]
+        public string UserName { get; set; }
+        [Required]
+        [DisplayName("Password")]
+        public string Password { get; set; }
+        [EmailAddress]
+        public string? EmailAddress { get; set; }
+    }
+}
