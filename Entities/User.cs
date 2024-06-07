148b46bc1844b536cf02c8cffb75a92618d81a66
Кирилл Солдатенко <soldatenko.kirillas@gmail.com>
2024-06-07 21:33:55 +0300

    add! : added Jwt refresh token

--- Entities/User.cs
+++ Entities/User.cs
@@ -9,5 +9,7 @@ namespace EventSeller.DataLayer.Entities
 {
     public class User : IdentityUser
     {
+        public string? RefreshToken { get; set; }
+        public DateTime RefreshTokenExpiryTime { get; set; }
     }
 }
