e8451bac3fd1c7754ac08357f9088bc96e865f41
Кирилл Солдатенко <soldatenko.kirillas@gmail.com>
2024-06-27 14:20:38 +0300

    info: add Unreturnable fee and User FK for ticket transaction

--- EF/Migrations/20240627111902_AddUnreturnableFeeForTicketTransactionAndUserFK.Designer.cs
+++ EF/Migrations/20240627111902_AddUnreturnableFeeForTicketTransactionAndUserFK.Designer.cs
@@ -4,6 +4,7 @@ using DataLayer.Model.EF;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EventSeller.Migrations
 {
     [DbContext(typeof(SellerContext))]
-    partial class SellerContextModelSnapshot : ModelSnapshot
+    [Migration("20240627111902_AddUnreturnableFeeForTicketTransactionAndUserFK")]
+    partial class AddUnreturnableFeeForTicketTransactionAndUserFK
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
