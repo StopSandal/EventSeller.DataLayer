4212a5bb3045cf4caae681049a6e276683ac53ab
Кирилл Солдатенко <soldatenko.kirillas@gmail.com>
2024-07-01 18:30:51 +0300

    feat: add TicketRegistration controller

--- Entities/Ticket.cs
+++ Entities/Ticket.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Data;
+using System.Text.Json.Serialization;
 
 namespace DataLayer.Model
 {
@@ -18,10 +19,13 @@ namespace DataLayer.Model
         public DateTime? TicketStartDateTime { get; set; }
         public DateTime? TicketEndDateTime { get; set; }
         public long? SeatID { get; set; }
+        [JsonIgnore]
         public virtual TicketSeat? Seat { get; set; }
         public long? HallID { get; set; }
+        [JsonIgnore]
         public virtual PlaceHall? Hall { get; set; }
         public long EventID { get; set; }
+        [JsonIgnore]
         public virtual Event Event { get; set; } 
     }
 }
