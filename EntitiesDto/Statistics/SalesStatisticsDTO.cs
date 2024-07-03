f88e05ef9f224594481b531e53bc6671c343d9a4
Кирилл Солдатенко <soldatenko.kirillas@gmail.com>
2024-07-03 23:03:33 +0300

    feat: add repository changes, start adding statictic for ticket selling

---
+++ EntitiesDto/Statistics/SalesStatisticsDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventSeller.DataLayer.EntitiesDto.Statistics
+{
+    public class SalesStatisticsDTO
+    {
+        public int TotalTickets { get; set; }
+        public int Sold { get; set; }
+
+    }
+}
