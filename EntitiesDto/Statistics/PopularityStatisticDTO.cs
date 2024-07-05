65c71dba92529fefacb4da9850d4c30641f2338c
Кирилл Солдатенко <soldatenko.kirillas@gmail.com>
2024-07-05 22:13:25 +0300

    feat: start adding ticket polpulariru statistic

---
+++ EntitiesDto/Statistics/PopularityStatisticDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventSeller.DataLayer.EntitiesDto.Statistics
+{
+    public class PopularityStatisticDTO
+    {
+        public decimal Popularity { get; set; }
+        public decimal Realization { get; set; }
+        public object StatisticCheckObject { get; set; }
+
+    }
+}
