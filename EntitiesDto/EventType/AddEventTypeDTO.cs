e7ff497dae6ca93c73179abcf19f95349c8361c8
Кирилл Солдатенко <soldatenko.kirillas@gmail.com>
2024-07-05 22:28:38 +0300

    feat: add EventType Service

---
+++ EntitiesDto/EventType/AddEventTypeDTO.cs
@@ -0,0 +1,22 @@
+﻿using DataLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventSeller.DataLayer.EntitiesDto.EventType
+{
+    public class AddEventTypeDTO
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int MinutesForMoneyReturn { get; set; }
+        [Required]
+        public decimal BookingFeePercentage { get; set; }
+        public int? MinimalSeatRowForEvent { get; set; } = null;
+    }
+}
---
+++ EntitiesDto/EventType/EditEventTypeDTO.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace EventSeller.DataLayer.EntitiesDto.EventType
+{
+    public class EditEventTypeDTO
+    {
+        public string? Name { get; set; }
+        public int? MinutesForMoneyReturn { get; set; }
+        public decimal? BookingFeePercentage { get; set; }
+        public int? MinimalSeatRowForEvent { get; set; } = null;
+    }
+}
