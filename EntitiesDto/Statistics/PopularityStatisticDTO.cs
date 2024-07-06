c1bce19dba67716adc22c072bd095e38c7f0dad7
Кирилл Солдатенко <soldatenko.kirillas@gmail.com>
2024-07-06 23:25:30 +0300

    feat: reworked popularity meashures

--- EntitiesDto/Statistics/PopularityStatisticDTO.cs
+++ EntitiesDto/Statistics/PopularityStatisticDTO.cs
@@ -4,7 +4,10 @@ namespace EventSeller.DataLayer.EntitiesDto.Statistics
     public class PopularityStatisticDTO
     {
         public decimal Popularity { get; set; }
+        public decimal Monetization { get; set; }
         public decimal Realization { get; set; }
+        public decimal TotalIncome { get; set; }
+        public int TotalSold { get; set;}
 
     }
 }
