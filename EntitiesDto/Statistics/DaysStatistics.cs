0cbcaa4cc16dcbf48192356316c82a30e26cbfbb
Кирилл Солдатенко <soldatenko.kirillas@gmail.com>
2024-07-07 21:29:21 +0300

    Feat: added Dayily statistic

---
+++ EntitiesDto/Statistics/DaysStatistics.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventSeller.DataLayer.EntitiesDto.Statistics
+{
+    public class DaysStatistics
+    {
+        public DateTime Date {  get; set; }
+        public string DayOfWeek { get; set; } = "";
+        public int TotalTraffic {  get; set; }
+    }
+}
