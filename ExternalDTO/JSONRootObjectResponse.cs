cbc51ec9091c367bea9c10cdcb43005bc5fdf655
Кирилл Солдатенко <soldatenko.kirillas@gmail.com>
2024-07-11 17:16:32 +0300

    refactor: renamed constants, separated general contants to file

--- ExternalDTO/JSONRootObjectResponse.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace EventSeller.DataLayer.ExternalDTO
-{
-    public class JSONRootObjectResponse<TClass> where TClass : class
-    {
-        public TClass ProcessPaymentResponse { get; set; }
-    }
-}
---
+++ ExternalDTO/ProcessPaymentResponseRootObject.cs
@@ -0,0 +1,9 @@
+﻿using EventSeller.DataLayer.ExternalDTO.PaymentSystem;
+
+namespace EventSeller.DataLayer.ExternalDTO
+{
+    public class ProcessPaymentResponseRootObject
+    {
+        public ProcessPaymentResponse ProcessPaymentResponse { get; set; }
+    }
+}
